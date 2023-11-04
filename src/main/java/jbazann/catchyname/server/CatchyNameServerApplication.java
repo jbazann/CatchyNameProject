2661c5ab3f9665e815306799925b9670291660cf
jbazann <jbazan2@hotmail.com>
2023-11-04 14:56:16 -0300

    Spring Boot template

---
+++ src/main/java/jbazann/catchyname/server/CatchyNameServerApplication.java
@@ -0,0 +1,13 @@
+package jbazann.catchyname.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchyNameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchyNameServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jbazann/catchyname/server/CatchyNameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jbazann.catchyname.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchyNameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
