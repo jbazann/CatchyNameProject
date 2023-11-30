88fbf2dd4598041d3034bbfd800a8d0d5d8f9d06
jbazann <jbazan2@hotmail.com>
2023-11-30 03:39:41 -0300

    Parameterize file paths

--- src/main/java/jbazann/catchyname/server/home/HomeController.java
+++ src/main/java/jbazann/catchyname/server/home/HomeController.java
@@ -1,13 +1,19 @@
 package jbazann.catchyname.server.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public final class HomeController {
     
     @RequestMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("mainScript", "js/sheet.js");
+        model.addAttribute("indexCss","css/index.css");
+        model.addAttribute("topbarCss","css/topbar.css");
+        model.addAttribute("sheetCss","css/sheet.css");
+
         return "index";
     }
 }
